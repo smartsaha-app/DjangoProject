867bb44863e299c9607833ae0084d6f1a8748eb0
rajaonera <hugueszeus@gmail.com>
2025-09-03 15:27:16 +0300

    Add support for crop management and parcel-crop associations
    - Refactored `Crop` model to simplify data structure and removed redundant fields.
    - Introduced `ParcelCrop` model to link crops, parcels, and their statuses.
    - Enhanced `Parcel` model with updated `ordering` options.
    - Seeded initial data for crop statuses (`StatusCrop`) and crop varieties (`Variety`).
    - Implemented serializers and viewsets for `Crop`, `StatusCrop`, `Variety`, and `ParcelCrop`.
    - Updated API routes to include endpoints for crop and parcel-crop management.
    - Added example HTTP requests for managing crop-related entities.

--- SmartSaha/models/__init__.py
+++ SmartSaha/models/__init__.py
@@ -1,5 +1,5 @@
 from .users import User
-from .cultures import Crop, Variety, StatusCrop
+from .cultures import Crop, Variety, StatusCrop, ParcelCrop
 from .messages import Message
 from .parcels import Parcel, ParcelPoint
 from .tasks import Task, TaskStatus,TaskPriority
--- SmartSaha/views/__init__.py
+++ SmartSaha/views/__init__.py
@@ -1,2 +1,3 @@
 from .parcels import ParcelCreateView,ParcelViewSet, ParcelPointViewSet
 from  .users import UserViewSet, LoginView, SignupView,ForgotPasswordView,ResetPasswordView
+from .cultures import CropViewSet,VarietyViewSet,StatusCropViewSet,ParcelCropViewSet
