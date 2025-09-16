2e90005477905b4fcb0603a29125efad3fae8397
rajaonera <hugueszeus@gmail.com>
2025-09-16 16:13:43 +0300

    Add `parcel_full_data_page` template and extend imports for parcel and task views
    - Introduced `parcel_full_data.html` template for detailed dashboard rendering.
    - Extended imports in `views/__init__.py` to include `parcel_full_data_page`, `assistant_agronome_page`, and `dashboard`.
    - Enhanced dashboard features with added template styling and interactivity.

--- SmartSaha/views/__init__.py
+++ SmartSaha/views/__init__.py
@@ -1,9 +1,10 @@
-from .parcels import ParcelCreateView,ParcelViewSet, ParcelPointViewSet, ParcelFullDataViewSet
+from .parcels import ParcelCreateView,ParcelViewSet, ParcelPointViewSet, ParcelFullDataViewSet, parcel_full_data_page
 from  .users import UserViewSet, LoginView, SignupView,ForgotPasswordView,ResetPasswordView
 from .cultures import CropViewSet,VarietyViewSet,StatusCropViewSet,ParcelCropViewSet
-from .tasks import TaskViewSet, TaskStatusViewSet , TaskPriorityViewSet
+from .tasks import TaskViewSet, TaskStatusViewSet , TaskPriorityViewSet, tasks_view
 from .yelds import YieldRecordViewSet,YieldAnalyticsView
 from .externalData import SoilDataView,ClimateDataView, DataViewSet
-from .deepseek import AgronomyAssistantAPIView
+from .deepseek import AgronomyAssistantAPIView, assistant_agronome_api, assistant_agronome_page
 from .forecast import YieldForecastView
+from .dashboard import dashboard
 from .dashboard import DashboardViewSet
