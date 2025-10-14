b17d9a3e882ff7c2756f1978d41326beb52886d3
rajaonera <hugueszeus@gmail.com>
2025-10-14 07:45:46 +0300

    Update `settings.py` to switch database configuration to local PostgreSQL and adjust static files path
    - Enabled local database settings with PostgreSQL for development.
    - Modified `STATICFILES_DIRS` to use the updated static directory path (`smartSaha/static`).

--- SmaartSahaProject/settings.py
+++ SmaartSahaProject/settings.py
@@ -18,13 +18,13 @@ load_dotenv()
 import dj_database_url
 import os
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
 
 import os
 from pathlib import Path
@@ -109,7 +109,8 @@ TEMPLATES = [
 ]
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "smartSaha/static"]
 
 WSGI_APPLICATION = 'SmaartSahaProject.wsgi.application'
 
@@ -117,16 +118,16 @@ WSGI_APPLICATION = 'SmaartSahaProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'geodb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geodb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
